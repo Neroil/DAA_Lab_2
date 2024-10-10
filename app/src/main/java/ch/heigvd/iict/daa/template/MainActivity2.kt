3d9ca2ad0b4442a92814d74cd288a72077cece0c
GuillaumeDnt2 <guillaume.dunant@heig-vd.ch>
2024-10-10 16:29:48 +0200

    Fragments

---
+++ app/src/main/java/ch/heigvd/iict/daa/template/MainActivity2.kt
@@ -0,0 +1,18 @@
+package ch.heigvd.iict.daa.template
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+    }
+
+}
